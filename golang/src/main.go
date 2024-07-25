c84405938c08851a470203ea00b8099c6b96e5cd
Tai Le <ltquoctaidn98@gmail.com>
2024-07-25 21:13:21 +0700

    feat: add handler to stream callbacks

--- golang/src/main.go
+++ golang/src/main.go
@@ -1,39 +1,43 @@
 package main
 
+import (
+	"arbitrage-bot/helpers"
+	CEX "arbitrage-bot/sourceProvider/cex"
+	"time"
+)
+
 func main() {
-    // binanceSP := sourceProvider.BinanceSourceProvider{}
-    // tokenList, err := binanceSP.GetTokenList(false)
-
-    // if err != nil {
-    //     fmt.Println(err)
-    //     return
-    // }
-
+    binanceSP := CEX.BinanceSourceProvider{}
+    tokenList, err := binanceSP.GetTokenList(false)
+    helpers.Panic(err)
+    _ = tokenList
     // fmt.Println(tokenList)
 
-    // symbol1 := sourceProvider.Symbol{
-    //     Symbol: "BTCUSDT",
-    //     BaseAsset: "BTC",
-    //     QuoteAsset: "USDT",
-    // }
-    // symbol2 := sourceProvider.Symbol{
-    //     Symbol: "ETHUSDT",
-    //     BaseAsset: "ETH",
-    //     QuoteAsset: "USDT",
-    // }
-    // binanceSourceProvider := sourceProvider.BinanceSourceProvider{}
-    // binanceSourceProvider.SubscribeSymbol(symbol1)
-    // binanceSourceProvider.SubscribeSymbol(symbol2)
-    // var sec int = 0
+    symbol1 := CEX.Symbol{
+        Symbol: "STRKUSDT",
+        BaseAsset: "STRK",
+        QuoteAsset: "USDT",
+    }
+    symbol2 := CEX.Symbol{
+        Symbol: "BTCUSDT",
+        BaseAsset: "BTC",
+        QuoteAsset: "USDT",
+    }
+    binanceSourceProvider := CEX.NewBinanceSourceProvider()
+    binanceSourceProvider.SubscribeSymbol(symbol1)
+    binanceSourceProvider.SubscribeSymbol(symbol2)
+    var sec int = 0
 
-    // for {
-    //     time.Sleep(1 * time.Second)
-    //     sec++
+    for {
+        time.Sleep(1 * time.Second)
+        sec++
 
-    //     if sec > 20 {
-    //         break
-    //     }
-    // }
+        if sec > 20 {
+            break
+        }
+    }
 
-    // binanceSourceProvider.UnsubscribeSymbol(symbol1)
+    binanceSourceProvider.UnsubscribeSymbol(symbol1)
+
+    for {}
 }
