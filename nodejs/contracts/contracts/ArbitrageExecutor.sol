07b4fad498ce29fba7391ab2352e76e7002a0a29
Tai Le <ltquoctaidn98@gmail.com>
2024-09-26 21:20:39 +0700

    feat: deploy contract

--- nodejs/contracts/contracts/ArbitrageExecutor.sol
+++ nodejs/contracts/contracts/ArbitrageExecutor.sol
@@ -34,6 +34,8 @@ contract ArbitrageExecutor is Ownable {
     uint256 private constant MAX_UINT = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
     address public immutable PANCAKE_FACTORY;
     address public immutable PANCAKE_ROUTER;
+    address public immutable UNISWAP_V2_FACTORY;
+    address public immutable UNISWAP_V2_ROUTER;
 
     struct SwapParams {
         uint8 protocol;  // 0: PancakeSwap, 1: UniswapV2, 2: UniswapV3
@@ -42,19 +44,22 @@ contract ArbitrageExecutor is Ownable {
         uint24 fee;  // Only for UniswapV3
     }
 
-    constructor(address _pancakeFactory, address _pancakeRouter) {
+    constructor(address _pancakeFactory, address _pancakeRouter, address _uniswapV2Factory, address _uniswapV2Router) {
         owner = msg.sender;
         PANCAKE_FACTORY = _pancakeFactory;
         PANCAKE_ROUTER = _pancakeRouter;
+        UNISWAP_V2_FACTORY = _uniswapV2Factory;
+        UNISWAP_V2_ROUTER = _uniswapV2Router;
     }
 
     function swapIn(SwapParams[] calldata paramsArray, uint256 amountIn, address flashloanToken1) public {
         // swapIn with Flashloan, remember to set allowance for the tokens
         require(paramsArray.length > 0, "Empty params array");
         SwapParams calldata swapParams = paramsArray[0];
+        address factoryAddress = swapParams.protocol == 0 ? PANCAKE_FACTORY : UNISWAP_V2_FACTORY;
 
-        if (swapParams.protocol == 0) {
-            address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(swapParams.tokenIn, flashloanToken1);
+        if (swapParams.protocol == 0 || swapParams.protocol == 1) {
+            address pair = IUniswapV2Factory(factoryAddress).getPair(swapParams.tokenIn, flashloanToken1);
             require(pair != address(0), "This pool does not exist");
             (uint256 amount0Out, uint256 amount1Out) = IUniswapV2Pair(pair).token0() == swapParams.tokenIn
                 ? (amountIn, uint256(0))
@@ -82,13 +87,29 @@ contract ArbitrageExecutor is Ownable {
         uint256 fee = ((amountIn * 3) / 997) + 1;
         uint256 amountToRepay = amountIn + fee;
         uint256 loanAmount = _amount0 > 0 ? _amount0 : _amount1;
+        uint256 amountOut = placeTradeUniswapV2(paramsArray, loanAmount, PANCAKE_ROUTER);
+        require(amountOut > amountToRepay, "Arbitrage not profitable");
+        // Pay loan back
+        IERC20(paramsArray[0].tokenIn).safeTransfer(pair, amountToRepay);
+    }
 
-        if (paramsArray[0].protocol == 0) {
-            uint256 amountOut = placeTradeUniswapV2(paramsArray, loanAmount, PANCAKE_ROUTER);
-            require(amountOut > amountIn, "Arbitrage not profitable");
-            // Pay loan back
-            IERC20(paramsArray[0].tokenIn).safeTransfer(pair, amountToRepay);
-        }
+    function uniswapV2Call(address _sender, uint256 _amount0, uint256 _amount1, bytes calldata _data) external {
+        // Ensure this request comes from the pair pool contract
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        address pair = IUniswapV2Factory(UNISWAP_V2_FACTORY).getPair(token0, token1);
+        require(msg.sender == pair, "The sender needs to match the pair");
+        require(_sender == address(this), "Sender should match this contract");
+
+        (SwapParams[] memory paramsArray, uint256 amountIn) = abi.decode(_data, (SwapParams[], uint256));
+        // Calculate the amount to repay at the end
+        uint256 fee = ((amountIn * 3) / 997) + 1;
+        uint256 amountToRepay = amountIn + fee;
+        uint256 loanAmount = _amount0 > 0 ? _amount0 : _amount1;
+        uint256 amountOut = placeTradeUniswapV2(paramsArray, loanAmount, UNISWAP_V2_ROUTER);
+        require(amountOut > amountToRepay, "Arbitrage not profitable");
+        // Pay loan back
+        IERC20(paramsArray[0].tokenIn).safeTransfer(pair, amountToRepay);
     }
 
     function placeTradeUniswapV2(
--- nodejs/contracts/hardhat.config.js
+++ nodejs/contracts/hardhat.config.js
@@ -1,5 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
 
+const PRIVATE_KEY_TEST = '1bc10d8ef0bdeb8aa2eeaafe281fc5f58527256fbb53a58ea9020d65f684e674';
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -13,16 +15,47 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: 'https://bsc-mainnet.infura.io/v3/dd7bf6951267477f8f20d852e0135f84',
+        url: 'https://bsc-dataseed1.binance.org/',
+        chainId: 56,
         // blockNumber: 42163104, // block number of the latest block at the moment (without this, it gets error)
       },
     },
-    testnet: {
+    bscTestnet: {
       url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       chainId: 97,
-      accounts: [],
+      accounts: [
+        PRIVATE_KEY_TEST,
+      ],
+      dex: {
+        pancake: {
+          factory: '0xB7926C0430Afb07AA7DEfDE6DA862aE0Bde767bc',
+          router: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
+        },
+        uniswapV2: {
+          factory: '0x0000000000000000000000000000000000000000',
+          router: '0x0000000000000000000000000000000000000000',
+        },
+      }
     },
-    mainnet: {
+    sepolia: {
+      url: 'https://sepolia.infura.io/v3/dd7bf6951267477f8f20d852e0135f84',
+      chainId: 11155111,
+      accounts: [
+        PRIVATE_KEY_TEST,
+      ],
+      dex: {
+        // Only PancakeSwapV3 is supported on Sepolia
+        pancake: {
+          factory: '0x0000000000000000000000000000000000000000',
+          router: '0x0000000000000000000000000000000000000000',
+        },
+        uniswapV2: {
+          factory: '0x7E0987E5b3a30e3f2828572Bb659A548460a3003',
+          router: '0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008',
+        },
+      }
+    },
+    bscMainnet: {
       url: 'https://bsc-dataseed1.binance.org/',
       chainId: 56,
       accounts: [],
